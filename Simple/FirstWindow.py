158ed385aaf778e8437aa57ed9ac5d037385a43d
Navid <navidnasyry@gmail.com>
2021-02-12 12:18:24 +0330

    mybe plus end

--- Simple/FirstWindow.py
+++ Simple/FirstWindow.py
@@ -9,7 +9,11 @@ class Ui_FirstWindow(QMainWindow):
         uic.loadUi('FirstWindow.ui' , self)
         self.result=0
         self.result_str_all=""
+        self.startCall()
+        self.last_operator="+"
 
+
+    def startCall(self):
         self.btn_1.clicked.connect(lambda :self.enterNum(1))
         self.btn_0.clicked.connect(lambda :self.enterNum(0))
         self.btn_2.clicked.connect(lambda :self.enterNum(2))
@@ -20,21 +24,41 @@ class Ui_FirstWindow(QMainWindow):
         self.btn_7.clicked.connect(lambda :self.enterNum(7))
         self.btn_8.clicked.connect(lambda :self.enterNum(8))
         self.btn_9.clicked.connect(lambda :self.enterNum(9))
-
+        self.btn_plus.clicked.connect(lambda :self.enterOperator('+' , True))
+        self.btn_equal.clicked.connect(lambda :self.enterOperator('=' , False))
 
 
 
 
     def enterNum(self , num):
-        self.result_str_all+=str(num)
         str_now = self.led_show.text()
         self.led_show.setText(str_now + str(num))
+        self.result_str_all+=str(num)
+        self.led_show_all.setText(self.result_str_all)
 
 
-    def enterOperantor(self , op_str):
-        pass
+    def enterOperator(self , op_str , end_operator):
+        if op_str == '+':
+            self.last_operator= '+'
 
+            input_float = float(self.led_show.text())
+            self.result += input_float
+            self.dsb_result.setValue(self.result)
+            if end_operator:
+                self.result_str_all+=' '+op_str+' '
+            self.led_show_all.setText(self.result_str_all)
+            self.led_show.clear()
 
+        elif op_str=='-':
+            pass
+        elif op_str=='/':
+            pass
+        elif op_str=='*':
+            pass
+
+        elif op_str=='=':
+            self.last_operator = '='
+            self.enterOperator(self.last_operator, False)
 
 
 
@@ -50,6 +74,6 @@ class Ui_FirstWindow(QMainWindow):
        self.first_btn.move(10 , 20)
        self.first_btn.clicked.connect(self.clicked)
 
-   def clicked(self):
+    def clicked(self):
        self.first_lbl.setText("clicked")
 '''
